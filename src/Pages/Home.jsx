1061695a07ffba21a00914c189a541c8a2c78a01
tejas <sadadetej.27@gmail.com>
2023-07-12 01:46:23 +0530

    Logout button added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 import classes from './Home.module.css'
 import { useNavigate } from 'react-router-dom'
 import LoggedIn from './LoggedIn';
+import AuthContext from '../LoginStore/Auth-context';
+import { useContext } from 'react';
 
 function Home() {
+  const authCtx= useContext(AuthContext);
   const navigate=useNavigate();
   const nextPageHandler = (event) => {
     navigate('/submitdetails')
+  }
+
+  const logoutHandler = () => {
+     authCtx.logout();
   }
 
   return (
@@ -16,6 +23,7 @@ function Home() {
         <h3> Welcome to expense tracker</h3>
 
         <div className={classes.headerdiv}>
+          <li><button onClick={logoutHandler}>Logout</button></li>
         <li> <h4>Your Profile is incomplete</h4> </li>
         <li className={classes.link} onClick={nextPageHandler}> <h4>Complete now</h4></li>
         </div>
